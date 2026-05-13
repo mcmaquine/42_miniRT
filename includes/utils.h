a2bd3e0c68daaee602b9e2dce358b8653c480c7a
GabrielGem <gabrielgarciamendes91@gmail.com>
2026-05-13 19:44:11 -0300

    add: function pointer

--- includes/utils.h
+++ includes/utils.h
@@ -6,12 +6,14 @@
 /*   By: mmaquine <mmaquine@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/30 17:46:58 by mmaquine          #+#    #+#             */
-/*   Updated: 2026/04/07 15:53:30 by gabrgarc         ###   ########.fr       */
+/*   Updated: 2026/05/13 18:45:56 by gabrgarc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef UTILS_H
 # define UTILS_H
+
+typedef void	(*t_objs)(t_scene_obj *obj);
 
 REAL to_radians(REAL degree);
 #endif
