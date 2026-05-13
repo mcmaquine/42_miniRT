a3ceacb40e792126ef47680dc849a74f978a2296
mmaquine <mmaquine@student.42sp.org.br>
2026-05-13 19:53:37 -0300

    fixing cylinder colisions

--- includes/raytracer.h
+++ includes/raytracer.h
@@ -6,14 +6,13 @@
 /*   By: mmaquine <mmaquine@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/01 18:38:57 by mmaquine          #+#    #+#             */
-/*   Updated: 2026/04/29 14:19:17 by mmaquine         ###   ########.fr       */
+/*   Updated: 2026/05/12 17:46:03 by mmaquine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef RAY_TRACER_H
 # define RAY_TRACER_H
 
-t_plane *find_planes(t_window *win);
 t_ray	generate_ray(t_window *win, int px, int py);
 REAL	intersect_plane(t_window *win, t_ray ray);
 REAL	intersect_sphere(t_window *win, t_ray ray);
--- srcs/raytracing/cyl_intersec.c
+++ srcs/raytracing/cyl_intersec.c
@@ -6,7 +6,7 @@
 /*   By: mmaquine <mmaquine@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/27 18:41:54 by mmaquine          #+#    #+#             */
-/*   Updated: 2026/05/12 15:12:02 by gabrgarc         ###   ########.fr       */
+/*   Updated: 2026/05/13 19:50:06 by mmaquine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,36 +14,8 @@
 
 static	REAL	full_intersection(t_cylinder *sph, t_ray ray);
 static	REAL	check_height_intersec(t_cylinder *cyl, t_ray r, REAL t);
-
-/*
-t_cylinder	**find_cylinders(t_window *win)
-{
-	int			count;
-	t_list		*objs;
-	t_cylinder	**cyl;
-
-	count = 0;
-	objs = win->scene_obj.objs;
-	while (objs)
-	{
-		if (((t_scene_obj *)(objs->content))->type == CYLINDER)
-			count++;
-		objs = objs->next;
-	}
-	cyl = ft_calloc(count + 1, sizeof(t_cylinder *));
-	if (!cyl)
-		return (NULL);
-	objs = win->scene_obj.objs;
-	count = 0;
-	while (objs)
-	{
-		if (((t_scene_obj *)(objs->content))->type == CYLINDER)
-			cyl[count++] = (t_cylinder *)(objs->content);
-		objs = objs->next;
-	}
-	return (cyl);
-}
-*/
+static	REAL	check_face_intersec(t_plane *p, REAL r_sq, t_ray r);
+static	REAL	check_tube_intersec(t_cylinder *cyl, t_ray ray);
 
 REAL	intersect_cylinder(t_window *win, t_ray ray)
 {
@@ -70,7 +42,25 @@ REAL	intersect_cylinder(t_window *win, t_ray ray)
 	return (-1);
 }
 
-static REAL	full_intersection(t_cylinder *cylinder, t_ray ray)
+static	REAL	full_intersection(t_cylinder *cyl, t_ray ray)
+{
+	REAL	t_top;
+	REAL	t_base;
+	REAL	t_tube;
+	REAL	t_small;
+
+	t_top = check_face_intersec(&(cyl->top), cyl->diam*.5, ray);
+	t_base = check_face_intersec(&(cyl->base), cyl->r_sq, ray);
+	t_tube = check_tube_intersec(cyl, ray);
+	t_small = DBL_MAX;
+	if (t_base < t_small && t_base > 0)
+		t_small = t_base;
+	if (t_small == -1 || (t_tube < t_small && t_tube > 0))
+		t_small = t_tube;
+	return (t_small);
+}
+
+static REAL	check_tube_intersec(t_cylinder *cyl, t_ray ray)
 {
 	REAL	dv_axis;
 	REAL	cov_axis;
@@ -79,16 +69,16 @@ static REAL	full_intersection(t_cylinder *cylinder, t_ray ray)
 	REAL	t;
 
 	t = DBL_MAX;
-	if (!cylinder)
+	if (!cyl)
 		return (-1);
-	dv_axis = vec_dot(ray.direction, cylinder->v_axis);
-	cov_axis = vec_dot(vec_sub(ray.origin, cylinder->center), cylinder->v_axis);
-	dco = vec_dot(vec_sub(ray.origin, cylinder->center), ray.direction);
-	co_sq = vec_magnitude(vec_sub(ray.origin, cylinder->center));
+	dv_axis = vec_dot(ray.direction, cyl->v_axis);
+	cov_axis = vec_dot(vec_sub(ray.origin, cyl->center), cyl->v_axis);
+	dco = vec_dot(vec_sub(ray.origin, cyl->center), ray.direction);
+	co_sq = vec_magnitude(vec_sub(ray.origin, cyl->center));
 	co_sq = co_sq * co_sq;
 	t = roots(1.0 - dv_axis*dv_axis, 2*(dco - dv_axis*cov_axis),\
-	co_sq - cov_axis*cov_axis - cylinder->diam * cylinder->diam * 0.25);
-	t = check_height_intersec(cylinder, ray, t);
+	co_sq - cov_axis*cov_axis - cyl->r_sq);
+	t = check_height_intersec(cyl, ray, t);
 	return (t);
 }
 
@@ -101,8 +91,35 @@ static	REAL	check_height_intersec(t_cylinder *cyl, t_ray r, REAL t)
 		return (t);
 	point = vec_add(r.origin, vec_scale(r.direction, t));
 	h = vec_dot(vec_sub(point, cyl->center), cyl->v_axis);
-	if (h > cyl->height / 2 || h < -cyl->height / 2)
+	if (h > cyl->height_2 || h < -cyl->height_2)
 		return (-1);
+	else
+		return (t);
+}
+
+static REAL	check_face_intersec(t_plane *p, REAL rad, t_ray r)
+{
+	REAL	dot;
+	REAL	t;
+	t_point	pt;
+	REAL	dist;
+
+	if (!p)
+		return (-1);
+	t = -1;
+	dot = vec_dot(p->normal, r.direction);
+	if (!ft_dcmp(dot, 0.0, 1e-5))
+		return (-1);
+	t = -vec_dot(p->normal, vec_sub(r.origin, p->a_point)) / dot;
+	if (t > 0)
+	{
+		pt = vec_add(r.origin , vec_scale(r.direction, t));
+		dist = vec_magnitude(vec_sub(pt, p->a_point));
+		if (dist <= rad)
+			return (t);
+		else
+			return (-1);
+	}
 	else
 		return (t);
 }
