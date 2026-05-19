46180269cc33539f2ae1b8f70edc607795a7b6b0
mcmaquine <mcmaquine@gmail.com>
2026-05-18 21:38:18 -0300

    fixed intersec_plane to return a struct that contains the lesser t, and the object hit, must implement other 2 objects

--- includes/raytracer.h
+++ includes/raytracer.h
@@ -6,7 +6,7 @@
 /*   By: mmaquine <mmaquine@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/01 18:38:57 by mmaquine          #+#    #+#             */
-/*   Updated: 2026/05/12 17:46:03 by mmaquine         ###   ########.fr       */
+/*   Updated: 2026/05/18 20:58:36 by mmaquine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,10 @@
 # define RAY_TRACER_H
 
 t_ray	generate_ray(t_window *win, int px, int py);
-REAL	intersect_plane(t_window *win, t_ray ray);
-REAL	intersect_sphere(t_window *win, t_ray ray);
-REAL	intersect_cylinder(t_window *win, t_ray);
+t_hit	intersect_plane(t_window *win, t_ray ray);
+t_hit	intersect_sphere(t_window *win, t_ray ray);
+t_hit	intersect_cylinder(t_window *win, t_ray);
 //utils
 REAL 	roots(REAL a, REAL b, REAL c);
+t_hit	init_t_hit(t_hit *hit);
 #endif
