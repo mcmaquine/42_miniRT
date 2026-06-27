32ca6cfe9befbd108a066ec8b72df44028d286d9
GabrielGem <gabrielgarciamendes91@gmail.com>
2026-06-27 16:18:07 -0300

    fix: pass windows prototypes for window header

--- includes/window.h
+++ includes/window.h
@@ -6,14 +6,17 @@
 /*   By: mmaquine <mmaquine@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/02 17:15:07 by mmaquine          #+#    #+#             */
-/*   Updated: 2026/03/02 17:21:47 by mmaquine         ###   ########.fr       */
+/*   Updated: 2026/06/27 11:51:09 by gabrgarc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef WINDOW_H
 # define WINDOW_H
 # include "minirt.h"
-void	*create_window(t_window *w, int width, int height);
+
+void	start_window(t_window *win, int width, int height);
+void	events(t_window *win);
 void	paint_pixel(t_window *w, int x, int y, unsigned int color);
 void	clear_canva(t_window *w);
+
 #endif
