550ce347782c33dce698477aef664373751119b4
GabrielGem <gabrielgarciamendes91@gmail.com>
2026-07-03 21:35:39 -0300

    feat: struct thread

--- include_bonus/threads.h
+++ include_bonus/threads.h
@@ -6,13 +6,20 @@
 /*   By: gabrgarc <gabrgarc@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/01 21:30:22 by gabrgarc          #+#    #+#             */
-/*   Updated: 2026/07/01 21:31:12 by gabrgarc         ###   ########.fr       */
+/*   Updated: 2026/07/03 21:27:38 by gabrgarc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef THREADS_H
 # define THREADS_H
 
-
+typedef struct	s_thread
+{
+	pthread_t	thread_id;
+	int			id;
+	char		*addr;
+	int			width;
+	int			height;
+}	t_thread;
 
 #endif
