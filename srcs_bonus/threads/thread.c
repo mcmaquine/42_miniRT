577add3cf84ed93490ac69865d2b1a20a31b7892
GabrielGem <gabrielgarciamendes91@gmail.com>
2026-07-03 21:37:03 -0300

    feat: function for init threads

--- srcs_bonus/threads/thread.c
+++ srcs_bonus/threads/thread.c
@@ -6,7 +6,7 @@
 /*   By: gabrgarc <gabrgarc@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/01 21:44:16 by gabrgarc          #+#    #+#             */
-/*   Updated: 2026/07/01 21:47:40 by gabrgarc         ###   ########.fr       */
+/*   Updated: 2026/07/03 21:29:49 by gabrgarc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,20 @@
 
 void	thread_init(t_window *win, int width, int height)
 {
+	t_thread	*threads;
+	int			num_threads;
+	int			range_height;
+	int			i;
 
+	num_threads = get_num_threads();
+	threads = ft_calloc(num_threads, sizeof(t_thread));
+	range_height = height / num_thread;
+	i = 0;
+	while (i < num_threads)
+	{
+		threads[i].id = i + 1;
+		threads[i]->addr = win->canva->addr[width * range_height * (i + 1)];
+		threads[i].width = width;
+		threads[i].height = range_height;
+	}
 }
