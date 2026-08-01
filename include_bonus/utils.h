7c92865bb51af305f6fb81383bc8630456e85f95
GabrielGem <gabrielgarciamendes91@gmail.com>
2026-08-01 10:01:07 -0300

    add: array and count objets prototypes

--- include_bonus/utils.h
+++ include_bonus/utils.h
@@ -6,7 +6,7 @@
 /*   By: mmaquine <mmaquine@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/30 17:46:58 by mmaquine          #+#    #+#             */
-/*   Updated: 2026/07/04 17:27:34 by gabrgarc         ###   ########.fr       */
+/*   Updated: 2026/07/31 22:06:53 by gabrgarc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,5 +21,7 @@ void	free_window(t_window *win);
 double	real_min(double x, double y, double z);
 double	real_max(double x, double y, double z);
 long	get_current_time(void);
+t_scene_obj	**array_objs(t_list *objs[], int n);
+int	count_objs(t_list *objs[]);
 
 #endif
